f9d78090e8eda13286c8febbe7a9458c96e5d27c
shyamji gupta <guptashyamji527@gmail.com>
2022-03-10 14:59:32 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 89;
+        int b = 98;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
